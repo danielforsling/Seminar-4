71fb79fac8df72b15a2e1f3a3e9ffc7b93996c4d
danielforsling <d.forsling@gmail.com>
2020-05-29 11:41:09 +0200

    Added exception description

--- PoS - sem 4/src/log/ExceptionLogHandler.java
+++ PoS - sem 4/src/log/ExceptionLogHandler.java
@@ -3,6 +3,9 @@ package log;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
 /**
  * 
@@ -22,6 +25,19 @@ public class ExceptionLogHandler {
 	}
 	    
 	public void printExceptionToFile(Exception e) {
-    	logFile.println(e.getMessage());
+    //	logFile.println(e.getMessage());
+    	StringBuilder logToBeSaved = new StringBuilder();
+    	logToBeSaved.append(createTime());
+        logToBeSaved.append(", Exception was thrown: ");
+        logToBeSaved.append(e.getMessage());
+        logFile.println(logToBeSaved);
+        e.printStackTrace(logFile);
     }
+
+    private String createTime() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM);
+        return now.format(formatter);
+    }
+    
 }

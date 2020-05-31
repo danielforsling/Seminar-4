f3a35bdc6e8405fd639a820a1a8ec7fd92417b2f
danielforsling <d.forsling@gmail.com>
2020-05-31 20:32:35 +0200

    Added comments and printCurrentState method

--- PoS - sem 4/src/view/TotalRevenueView.java
+++ PoS - sem 4/src/view/TotalRevenueView.java
@@ -4,20 +4,30 @@ import model.util.Amount;
 import model.util.TotalPriceObserver;
 
 /**
- * 
- *
+ *	Prints the totalPrice of the sale on a simulated external display.
  */
 public class TotalRevenueView implements TotalPriceObserver{
 	
+	private Amount totalPrice;
+	
 	/**
-	 *  Creates an instance.
+	 *  Creates an instance, set the field totalPrice to zero.
 	 */
 	public TotalRevenueView() {
-
+			totalPrice = new Amount(0);
 	}
 	
 	@Override
 	public void showTotalPrice(Amount totalPrice) {
+		this.totalPrice = this.totalPrice.addAmounts(totalPrice);
+		printCurrentState();
+	}
+	
+	private void printCurrentState() {
+		System.out.println();
+		System.out.println("******External Display******");
+		System.out.println("Total revenue: " + totalPrice.toString());
+		System.out.println("****************************");
 		
 	}
 
